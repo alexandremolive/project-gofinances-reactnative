5f7929c796d6a5382a4651aa07c5623e920554fc
Alexandre Oliveira <alexandremolive@gmail.com>
2021-10-05 10:59:20 -0300

    feat screen Register and Button Enviar

---
+++ src/components/Form/Input/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { TextInputProps } from 'react-native'
+
+type Props = TextInputProps;
+
+import { Container } from './styles'
+
+export function Input({ ...rest }: Props) {
+  return (
+    <Container {...rest} />
+  );
+}
