0284bc6fa08d9646e44120b252029a9996ec2e9f
Alexandre Oliveira <alexandremolive@gmail.com>
2021-10-10 12:50:15 -0300

    feat category component

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Input } from '../../components/Form/Input';
 import { Button } from '../../components/Form/Button';
+import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
+import { CategorySelect } from '../../components/Form/CategorySelect';
+
 import {
   Container,
   Header,
   Title,
   Form,
   Fields,
-
+  TransactionTypes,
 } from './styles'
 
 
 export function Register() {
+  const [transactionType, setTransactionType] = useState('');
+
+  const handleTransactionsTypeSelect = (type: 'up' | 'down') => {
+    setTransactionType(type)
+  }
   return (
     <Container>
       <Header>
@@ -26,6 +34,25 @@ export function Register() {
           <Input
             placeholder='Preço'
           />
+          <TransactionTypes>
+            <TransactionTypeButton
+              type='up'
+              title='Income'
+              onPress={() => handleTransactionsTypeSelect('up')}
+              isActive={transactionType === 'up'}
+            />
+            <TransactionTypeButton
+              type='down'
+              title='Outcome'
+              onPress={() => handleTransactionsTypeSelect('down')}
+              isActive={transactionType === 'down'}
+            />
+          </TransactionTypes>
+          <CategorySelect
+            title="Categoria"
+          />
+
+
         </Fields >
         <Button title='Enviar' />
       </Form >
