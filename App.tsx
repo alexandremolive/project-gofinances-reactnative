cee8779c736dd9cb97fbafca6e991ac998353ee5
Alexandre Oliveira <alexandremolive@gmail.com>
2021-10-20 11:18:36 -0300

    feat: navigation tabs

--- App.tsx
+++ App.tsx
@@ -9,8 +9,10 @@ import {
   Poppins_700Bold
 } from '@expo-google-fonts/poppins';
 
-import theme from './src/global/styles/theme'
-import { Register } from './src/screens/Register';
+import theme from './src/global/styles/theme';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { AppRoutes } from './src/routes/app.routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -26,8 +28,9 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-
-        <Register />
+        <NavigationContainer>
+          <AppRoutes />
+        </NavigationContainer>
       </ThemeProvider>
     </>
   );
